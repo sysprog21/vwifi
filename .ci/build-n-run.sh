cdf3e18e9d9c947d3c6028ae1c85f8494fe68a9c
Jim Huang <jserv.tw@gmail.com>
2022-03-27 01:08:58 +0800

    Enable GitHub Actions
    The CI pipeline can automate the validation for Linux kernel module
    builds along with coding style checks. However, it is unable to load
    vwifi kernel module because of incomplete kernel modules shipped with
    Azure packages.
    
      Installing Module cfg80211
      modprobe: FATAL: Module cfg80211 not found in directory /lib/modules/5.11.0-1028-azure
      Installing Module vwifi.ko
      insmod: ERROR: could not insert module vwifi.ko: Unknown symbol in module

---
+++ .ci/build-n-run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+function build_mod()
+{   
+    make all || exit 1
+}
+
+function run_tests()
+{
+    make check || exit 2
+}
+
+build_mod
+# vwifi is buidable but failed to load.
+#   modprobe: FATAL: Module cfg80211 not found in directory /lib/modules/5.11.0-1028-azure
+#   Installing Module vwifi.ko
+#   insmod: ERROR: could not insert module vwifi.ko: Unknown symbol in module
+# run_tests
