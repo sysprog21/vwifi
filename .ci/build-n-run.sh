952a2e9b53448bd95b3f416e9c7f41c6fb6fbfe8
Jim Huang <jserv@ccns.ncku.edu.tw>
2024-02-23 00:48:21 +0800

    CI: Bump action:checkout version

--- .ci/build-n-run.sh
+++ .ci/build-n-run.sh
@@ -11,7 +11,7 @@ function run_tests()
 }
 
 build_mod
-# vwifi is buidable but failed to load.
+# vwifi is buildable but failed to load.
 #   modprobe: FATAL: Module cfg80211 not found in directory /lib/modules/5.11.0-1028-azure
 #   Installing Module vwifi.ko
 #   insmod: ERROR: could not insert module vwifi.ko: Unknown symbol in module
